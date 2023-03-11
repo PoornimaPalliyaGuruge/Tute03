009ee5b2ca72beb323225aa657183a2601b7ec4a
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2023-03-11 17:54:01 +0530

    initial commit

---
+++ app/src/main/java/com/example/tute03/activity_display.kt
@@ -0,0 +1,23 @@
+package com.example.tute03
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class activity_display : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+
+        var txtAnswer = findViewById<TextView>(R.id.edt_answer)
+        txtAnswer.setText(intent.getDoubleExtra("answer",0.0).toString())
+        var btnBack = findViewById<Button>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
